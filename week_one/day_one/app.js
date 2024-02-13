fd7376105ad0036c77ce21da2dbb335d51d61283
MbuguaDennis <dmbugua.developer@gmail.com>
2024-02-13 16:19:22 +0300

    updated js file

---
+++ week_one/day_one/app.js
@@ -0,0 +1,19 @@
+ // This is inline comment in javascript
+  /*
+  This is a multiline comments Javascript 
+  */
+ 
+//   Javascript variables 
+
+
+let person1Age = 25; // number 
+let personName = " Danny Thompson"; //string  data type
+let amountCollected; //undefined 
+const personIsAvailabe = false; //boolean
+ 
+let meetingDate =null;  //null 
+
+
+
+ 
+
